14c6d20674996c354e5eb386caed683339f0a475
TayTroye <87233320+TayTroye@users.noreply.github.com>
2023-06-15 21:00:35 +0800

    Add files via upload

---
+++ 页面1/index1.js
@@ -0,0 +1,41 @@
+var click0_0 = function(){
+    document.getElementById('HPV-6').style.display='block';
+    document.getElementById('fade').style.display='block';
+}
+var click0_1 = function(){
+    document.getElementById('HPV-16').style.display='block';
+    document.getElementById('fade').style.display='block';
+}
+var click1 = function(){
+    document.getElementById('HPV-18').style.display='block';
+    document.getElementById('fade').style.display='block';
+}
+var closeit = function(){
+    document.getElementById('HPV-18').style.display='none';
+    document.getElementById('fade').style.display='none';
+}
+var  closeit0_0 = function(){
+    document.getElementById('HPV-6').style.display='none';
+    document.getElementById('fade').style.display='none';
+}
+var  closeit0_1 = function(){
+    document.getElementById('HPV-16').style.display='none';
+    document.getElementById('fade').style.display='none';
+}
+var  click1_1 = function(){
+    document.getElementById('HPV-13').style.display='block';
+    document.getElementById('fade').style.display='block';
+    console.log('click1_1');
+}
+var  closeit1_1 = function(){
+    document.getElementById('HPV-13').style.display='none';
+    document.getElementById('fade').style.display='none';
+}
+var click2_1 = function(){
+    document.getElementById('HPV-2').style.display='block';
+    document.getElementById('fade').style.display='block';
+}
+var  closeit2_1 = function(){
+    document.getElementById('HPV-2').style.display='none';
+    document.getElementById('fade').style.display='none';
+}
