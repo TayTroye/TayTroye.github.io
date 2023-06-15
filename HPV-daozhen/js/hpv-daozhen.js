80d4957b45f90d7eaa5212f19e3353c88eab4432
TayTroye <87233320+TayTroye@users.noreply.github.com>
2023-06-15 21:39:54 +0800

    Add files via upload

---
+++ HPV-daozhen/js/hpv-daozhen.js
@@ -0,0 +1,346 @@
+var yiyuanData = [
+    
+    {
+        // 数据1
+        shengfen: '四川',
+        yiyuanName: ['四川大学华西第二医院', '四川省人民医院','西南医科大学附属医院','成都市妇女儿童中心医院'],
+        yiyuansuozaidi:['成都','成都','泸州','成都'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['儿童医院','综合医院','综合医院','儿童医院'],
+        yiyuandianhua: ['028-85503960', '028-87769262','0830-31665200','028-87467888'],
+    },
+
+    {
+        // 数据2
+        shengfen: '北京',
+        yiyuanName: ['中国医学科学院北京协和医院', '首都医大附属北京地坛医院','首都医大附属北京妇产医院','中日友好医院'],
+        yiyuansuozaidi:['东城区','朝阳区','朝阳区','朝阳区'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '传染病医院','妇产医院','综合医院'],
+        yiyuandianhua: ['010-69155564', '010-84322200','010-52276699','010-84205288'],
+    },
+    {
+        // 数据3
+        shengfen: '天津',
+        yiyuanName: ['天津医科大学肿瘤医院', '天津市第一中心医院','天津市第五中心医院','天津市中心妇产科医院'],
+        yiyuansuozaidi:['河西区','南开区','塘沽区','南开区'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['肿瘤医院', '综合医院','综合医院','妇产医院'],
+        yiyuandianhua: ['022-23340123', '022-23626600','022-65665000','022-58287742'],
+    },
+    {
+        // 数据4
+        shengfen: '河北',
+        yiyuanName: ['河北北方医院附属第二医院', '河北医科大学第二医院','河北医科大学第三医院','衡水市人民医院'],
+        yiyuansuozaidi:['张家口','石家庄','石家庄','衡水市'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0313-3042224', '0311-87046901','0311-88603000','0318-2181234'],
+    },
+    {
+        // 数据5
+        shengfen: '山西',
+        yiyuanName: ['山西医科大学第一医院', '山西省人民医院','芮城县妇幼保健计划生育服务中心','山西省儿童医院'],
+        yiyuansuozaidi:['太原','太原','运城','太原'],
+        yiyuanJibie: ['三甲', '三甲','一级','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','妇幼保健院','儿童医院'],
+        yiyuandianhua: ['0351-4639114', '0351-4960081','0359-3032120','0351-3360752'],
+    },
+    {
+        // 数据6
+        shengfen: '内蒙古',
+        yiyuanName: ['内蒙古医科大学附属医院', '鄂尔多斯市中心医院','内蒙古自治区人民医院','呼和浩特市妇幼保健院'],
+        yiyuansuozaidi:['呼和浩特','鄂尔多斯','呼和浩特','呼和浩特'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三乙'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','妇幼保健院'],
+        yiyuandianhua: ['0471-3451345', '0477-8363180','0471-3283999','0471-2596006'],
+    },
+    {
+        // 数据7
+        shengfen: '辽宁省',
+        yiyuanName: ['中国医科大学附属第一医院', '中国医科大学附属盛京医院','辽宁省肿瘤医院','大连医科大学附属第一医院'],
+        yiyuansuozaidi:['沈阳','沈阳','沈阳','沈阳'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','肿瘤医院','综合医院'],
+        yiyuandianhua: ['024-961200', '024-96615','024-31916200','0411-83635963'],
+    },
+    {
+        // 数据8
+        shengfen: '吉林',
+        yiyuanName: ['吉林市中心医院', '吉林大学第二医院','吉林大学中日联谊医院','松原市中心医院'],
+        yiyuansuozaidi:['吉林','长春','长春','松原'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三级'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0432-62456181', '0431-81136555','0431-84995114','0438-6620004'],
+    },
+    {
+        // 数据9
+        shengfen: '黑龙江',
+        yiyuanName: ['哈尔滨医科大学附属第四医院', '哈尔滨市北方人民医院','大庆龙南医院','佳木斯大学附属第一医院'],
+        yiyuansuozaidi:['哈尔滨','哈尔滨','大庆','佳木斯'],
+        yiyuanJibie: ['三甲', '二级','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0451-82576888', '0451-83392615','0459-5989120','0454-8623698'],
+    },
+    {
+        // 数据10
+        shengfen: '上海',
+        yiyuanName: ['上海市第一妇婴保健院', '上海市第一人民医院','复旦大学附属妇产科医院','上海交大医学院附属仁济医院'],
+        yiyuansuozaidi:['浦东新区','虹口区','黄浦区','浦东新区'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['妇产医院', '综合医院','妇产医院','综合医院'],
+        yiyuandianhua: ['021-968886', '021-63240090','021-33189900','021-558752345'],
+    },
+    {
+        // 数据11
+        shengfen: '江苏',
+        yiyuanName: ['苏州大学附属第一医院', '南京鼓楼医院','苏北人民医院','南京市妇幼保健院'],
+        yiyuansuozaidi:['苏州','南京','扬州','南京'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','妇产医院'],
+        yiyuandianhua: ['0512-65223637', '025-83106666','0514-87373012','025-52226900'],
+    },
+    {
+        // 数据12
+        shengfen: '浙江',
+        yiyuanName: ['杭州市第一人民医院', '宁波市妇女儿童医院','温州医科大学附属第一医院','浙江大学医院附属第一医院'],
+        yiyuansuozaidi:['杭州','宁波','温州','杭州'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '妇产医院','综合医院','综合医院'],
+        yiyuandianhua: ['0571-56005600', '0574-87083300','0577-55579999','0571-87236114'],
+    },
+    {
+        // 数据13
+        shengfen: '安徽',
+        yiyuanName: ['中国科学技术大学附属第一医院', '芜湖市第一人民医院','安徽医科大学第二附属医院','铜陵市人民医院'],
+        yiyuansuozaidi:['合肥','芜湖','合肥','铜陵'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0551-62283114', '0553-2676102','0551-63869114','0562-5838240'],
+    },
+    {
+        // 数据14
+        shengfen: '福建',
+        yiyuanName: ['厦门市妇幼保健院', '福建医科大学附属第一医院','湖里区妇幼保健院','莆田市第一医院'],
+        yiyuansuozaidi:['厦门','福州','厦门','莆田'],
+        yiyuanJibie: ['三甲', '三甲','二级','三甲'],
+        yiyuanleixing: ['妇幼保健院', '综合医院','妇幼保健院','综合医院'],
+        yiyuandianhua: ['0592-2662020', '0591-87983333','0592-5507120','0594-8982060'],
+    },
+    {
+        // 数据15
+        shengfen: '江西',
+        yiyuanName: ['江西省妇幼保健院', '赣州市人民医院','赣南医学院第一附属医院','江西省皮肤病专科医院'],
+        yiyuansuozaidi:['南昌','赣州','赣州','南昌'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['妇产医院', '综合医院','综合医院','皮肤病医院'],
+        yiyuandianhua: ['0791-86224432', '0797-8083385','0797-8269564','0791-85214720'],
+    },
+    {
+        // 数据16
+        shengfen: '山东',
+        yiyuanName: ['山东大学齐鲁医院', '山东省立医院','青岛大学附属医院','山东省妇幼保健院'],
+        yiyuansuozaidi:['济南','济南','青岛','济南'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','妇幼保健院'],
+        yiyuandianhua: ['0531-82166666', '0531-68777114','0532-82911847','0531-68795005'],
+    },
+    {
+        // 数据17
+        shengfen: '河南',
+        yiyuanName: ['郑州大学第一附属医院', '河南省人民医院','商丘市第一人民医院','河南省中医院'],
+        yiyuansuozaidi:['郑州','郑州','商丘','郑州'],
+        yiyuanJibie:  ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','中医医院'],
+        yiyuandianhua: ['0371-66913114', '0371-65580014','0370-3255630','0371-60808781'],
+    },
+    {
+        // 数据18
+        shengfen: '湖北',
+        yiyuanName: ['华中科大同济医学院附属同济医院', '武汉大学人民医院','华中科大同济医学院附属协和医院','湖北省妇幼保健院'],
+        yiyuansuozaidi:['武汉','武汉','武汉','武汉'],
+        yiyuanJibie:['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','妇幼保健院'],
+        yiyuandianhua: ['027-83663131', '027-88041911','027-85726114','027-87862877'],
+    },
+    {
+        // 数据19
+        shengfen: '湖南',
+        yiyuanName: ['中南大学湘雅医院', '中南大学湘雅三医院','南华大学附属第一医院','湖南省妇幼保健院'],
+        yiyuansuozaidi:['长沙','长沙','衡阳','长沙'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','妇幼保健院'],
+        yiyuandianhua: ['0731-84328888', '0731-88638888','0734-8279467','0731-84332201'],
+    },
+    {
+        // 数据20
+        shengfen: '广东',
+        yiyuanName: ['中山大学孙逸仙纪念医院', '南方医科大学南方医院','广东省人民医院','中山大学附属第三医院'],
+        yiyuansuozaidi:['广州','广州','广州','广州'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['020-81332199', '020-61641888','020-83827812','020-85253333'],
+    },
+    {
+        // 数据21
+        shengfen: '广西',
+        yiyuanName: ['广西壮族自治区妇幼保健院', '广西医科大学第一附属医院','柳州市中医医院','广西医科大学附属肿瘤医院'],
+        yiyuansuozaidi:['南宁','南宁','柳州','南宁'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['妇幼保健院', '综合医院','中医医院','肿瘤医院'],
+        yiyuandianhua: ['0771-5802205', '0771-12580-6','0772-5357110','0771-5331955'],
+    },
+    {
+        // 数据22
+        shengfen: '海南',
+        yiyuanName: ['海南省妇女儿童医学中心', '琼海市中医院','中国人民解放军总医院海南医院',''],
+        yiyuansuozaidi:['海口','琼海','三亚',''],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['妇幼保健院', '中医医院','综合医院',''],
+        yiyuandianhua: ['0898-36373861', '0898-62822767','0898-38866666',''],
+    },
+    {
+        // 数据23
+        shengfen: '重庆',
+        yiyuanName: ['重庆市妇幼保健院', '重庆医科大学附属第二医院','重庆医科大学附属第一医院','陆军军医大学新桥医院'],
+        yiyuansuozaidi:['渝中区','渝中院区','渝中区','沙坪坝区'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['023-63702844', '023-63693222','023-63310999','023-68755000'],
+    },
+    {
+        // 数据24
+        shengfen: '贵州',
+        yiyuanName: ['贵州医科大学附属医院', '遵义医科大学附属医院','贵州省人民医院',''],
+        yiyuansuozaidi:['贵阳','遵义','贵阳',''],
+        yiyuanJibie: ['三甲', '三甲','三甲',''],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院',''],
+        yiyuandianhua: ['0851-86855119', '0851-28608114','0851-85922979',''],
+    },
+    {
+        // 数据25
+        shengfen: '云南',
+        yiyuanName: ['云南省肿瘤医院', '昆明医科大学第一附属医院','云南大学附属医院','昆明市第一人民医院'],
+        yiyuansuozaidi:['昆明','昆明','昆明','昆明'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['肿瘤医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0871-68189037', '0871-65324888','0871-65156650','0871-63188200'],
+    },
+    {
+        // 数据26
+        shengfen: '西藏',
+        yiyuanName: ['西藏自治区人民医院', '','',''],
+        yiyuansuozaidi:['拉萨','','',''],
+        yiyuanJibie: ['三甲', '','',''],
+        yiyuanleixing: ['综合医院', '','',''],
+        yiyuandianhua: ['0819-6371322', '','',''],
+    },
+    {
+        // 数据27
+        shengfen: '陕西',
+        yiyuanName: ['西京医院', '西安交通大学第一附属医院','西北妇女儿童医院','空军军医大学唐都医院'],
+        yiyuansuozaidi:['西安','西安','西安','西安'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','妇产医院','综合医院'],
+        yiyuandianhua: ['029-88886666', '029-85238888','029-89550001','029-8477777'],
+    },
+
+    {
+        // 数据28
+        shengfen: '甘肃',
+        yiyuanName: ['兰州第二医院', '兰州大学第一医院','庆阳市人民医院','平凉市第二人民医院'],
+        yiyuansuozaidi:['兰州','兰州','庆阳','平凉'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0931-8942262', '0931-8625200','0934-8232580','0933-8213455'],
+    },
+    {
+        // 数据29
+        shengfen: '青海',
+        yiyuanName: ['青海大学附属医院', '青海红十字医院','青海省人民医院','青海省中医院'],
+        yiyuansuozaidi:['西宁','西宁','西宁','西宁'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['0971-6162000', '0971-8247545','0971-8177911','0971-8456222'],
+    },
+    {
+        // 数据30
+        shengfen: '宁夏',
+        yiyuanName: ['宁夏医科大学总医院', '宁夏回族自治区妇幼保健院','银川市第一人民医院','宁夏医科大学总医院心脑血管病医院'],
+        yiyuansuozaidi:['银川','银川','银川','银川'],
+        yiyuanJibie: ['三甲', '二甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '妇产医院','综合医院','综合医院'],
+        yiyuandianhua: ['0951-6744457', '0951-7819587','0951-6997021','0951-4091488'],
+    },
+    {
+        // 数据31
+        shengfen: '新疆',
+        yiyuanName: ['乌鲁木齐妇幼保健院', '新疆医科大学第一附属医院','新疆维吾尔自治区人民医院','新建维吾尔自治区肿瘤医院'],
+        yiyuansuozaidi:['乌鲁木齐','乌鲁木齐','乌鲁木齐','乌鲁木齐'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['妇产医院', '综合医院','综合医院','肿瘤医院'],
+        yiyuandianhua: ['0991-8554659', '0991-4362974','0991-8562222','0991-7968027'],
+    },
+    {
+        // 数据32
+        shengfen: '香港',
+        yiyuanName: ['玛丽医院', '威尔斯亲王医院','香港伊利沙伯医院','养和医院'],
+        yiyuansuozaidi:['南区','新界','九龙','跑马地'],
+        yiyuanJibie: ['三甲', '三甲','三甲','三甲'],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院','综合医院'],
+        yiyuandianhua: ['00852-22553838', '00852-35052211','00852-35068888','00852-25720211'],
+    },
+    {
+        // 数据33
+        shengfen: '澳门',
+        yiyuanName: ['澳门仁伯爵综合医院', '澳门科大医院','澳门镜湖医院',''],
+        yiyuansuozaidi:['若宪马路','凼仔伟龙马路','镜湖马路',''],
+        yiyuanJibie: ['三甲', '三甲','三甲',''],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院',''],
+        yiyuandianhua: ['00853-28313731', '00853-28821838','00853-371333',''],
+    },
+    {
+        // 数据34
+        shengfen: '台湾',
+        yiyuanName: ['和信医院', '马偕纪念医院','台大医院',''],
+        yiyuansuozaidi:['台北市','台北市','台北市',''],
+        yiyuanJibie: ['三甲', '三甲','三甲',''],
+        yiyuanleixing: ['综合医院', '综合医院','综合医院',''],
+        yiyuandianhua: ['02-28970011', '02-25433535','02-23123456',''],
+    },
+    
+]
+
+var yiyuannames = document.getElementsByClassName('yiyuan-name');
+var yiyuansuozaidis = document.getElementsByClassName('yiyuan-suozaidi');
+var yiyuanjibies = document.getElementsByClassName('yiyuan-jibie');
+var yiyuanleixings = document.getElementsByClassName('yiyuan-leixing');
+var yiyuandianhuas = document.getElementsByClassName('yiyuan-dianhua');
+var yiyuandianhuaqianzhuis = document.getElementsByClassName('yiyuan-dianhua-qianzhui');
+
+$(".search-button").on("click", "img", function () {
+    search($("#search-text").val());
+})
+
+function search(value){
+    // 找到value对应的index
+    // alert($("#search-text").val());
+    for (let index = 0; index < 34; index++) {
+        if ($("#search-text").val()==yiyuanData[index].shengfen) {
+            replace(index);
+        }
+    }
+}
+//知道index后替换每个元素
+function replace(index) {
+    for (let i = 0; i < 4; i++) {
+        yiyuannames[i].innerHTML = yiyuanData[index].yiyuanName[i];
+        yiyuansuozaidis[i].innerHTML = yiyuanData[index].yiyuansuozaidi[i];
+        yiyuanjibies[i].innerHTML = yiyuanData[index].yiyuanJibie[i];
+        yiyuanleixings[i].innerHTML = yiyuanData[index].yiyuanleixing[i];
+        yiyuandianhuas[i].innerHTML = yiyuanData[index].yiyuandianhua[i];
+        if (yiyuandianhuas[i].innerHTML == '') {
+        yiyuandianhuaqianzhuis[i].innerHTML = '暂无更多推荐医院……';
+        }
+    }
+}
